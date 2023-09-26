b5b0c70b5681c3ef3336a05d7c925c4e7f62f89d
N <NagaArvind.N@ingrammicro.com>
2023-09-26 14:26:54 +0530

    restructured the files

---
+++ file.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/fileuploaded', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return 'success'
+
+if __name__ == '__main__':
+   app.run(debug = True)
