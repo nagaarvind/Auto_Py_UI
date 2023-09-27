0dc14572f263832cbf4f10cecda94ef7bce5e004
N <NagaArvind.N@ingrammicro.com>
2023-09-27 10:09:19 +0530

    changed the endpoint address

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
+import generate_graph
 
 app = Flask(__name__)
 
@@ -9,8 +10,11 @@ def index():
 @app.route('/fileuploaded', methods=['POST'])
 def upload_file():
     uploaded_file = request.files['file']
+    print(uploaded_file.filename)
+    
     if uploaded_file.filename != '':
         uploaded_file.save(uploaded_file.filename)
+    generate_graph.get_filename(uploaded_file.filename)
     return 'success'
 
 if __name__ == '__main__':
